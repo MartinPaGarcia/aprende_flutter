8fad0fbb4e1d048efb20b977d6308c5305faaaee
MartinPaGarcia <a01066569@itesm.mx>
2021-11-04 12:58:31 -0600

    Button Functionality for alert page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+
 class AlertPage extends StatelessWidget {
   const AlertPage({Key? key}) : super(key: key);
 
@@ -12,21 +15,48 @@ class AlertPage extends StatelessWidget {
         child: ElevatedButton(
           child: const Text("Mostrar Alerta"),
           style: ElevatedButton.styleFrom(
-            primary: Colors.green,
-            elevation: 5.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0)
-            )
-          ),
-          onPressed: (){},
+              primary: Colors.green,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0))),
+          onPressed: () => _mostrarAlert(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.door_back_door_rounded),
-        onPressed: (){
+        onPressed: () {
           Navigator.pop(context);
         },
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+            title: const Text("Titulo Alerta"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Este es el contenido de la alerta"),
+                FlutterLogo(size: 50.0)
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text("Cancelar"),
+                onPressed: ()=>Navigator.of(context).pop(),
+              ),
+              TextButton(
+                child: const Text("Ok"),
+                onPressed: ()=>Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
   }
 }
