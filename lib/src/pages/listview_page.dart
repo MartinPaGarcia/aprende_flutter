0acfe501f355f87f79180bcac8a8d48deb383a1b
MartinPaGarcia <a01066569@itesm.mx>
2021-11-05 16:32:44 -0600

    added: infinite scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,22 +1,64 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
 
-
 class ListaPage extends StatefulWidget {
-  const ListaPage({ Key? key }) : super(key: key);
+  const ListaPage({Key? key}) : super(key: key);
 
   @override
   _ListaPageState createState() => _ListaPageState();
 }
 
 class _ListaPageState extends State<ListaPage> {
+  ScrollController _scrollController = ScrollController();
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Pagina Lista"),
       ),
-      body: const Text("Listas"),
-      
+      body: _crearLista(),
     );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        // ignore: unused_local_variable
+        final imagen = _listaNumeros[index];
+
+        return FadeInImage(
+          image: NetworkImage("https://picsum.photos/500/300/?image=$index"),
+          placeholder: const AssetImage('assets/jar-loading.gif'),
+        );
+      },
+    );
+  }
+
+  void _agregar10() {
+    for (int i = 0; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
   }
 }
